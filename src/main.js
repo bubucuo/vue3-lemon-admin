9d73d653b75f9c1c29934eb6fb6336f721cee309
bubucuo <276185876@qq.com>
2025-05-07 18:12:58 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,12 +5,17 @@ import * as Icons from "@element-plus/icons-vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router/index.js";
-import { createPinia } from "pinia";
+// import { createPinia } from "pinia";
 
 const app = createApp(App);
 for (let i in Icons) {
   app.component(i, Icons[i]);
 }
+
+// ts
+// Object.entries(Icons).forEach(([key, component]) => {
+//   app.component(key, component as Component);
+// });
 // app.use(createPinia()).use(router).use(ElementPlus);
 
 app.use(router).use(ElementPlus);
